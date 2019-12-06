eca04665721a4c4210bb521b549a199bd2545579
mottox2 <y.mottox2@gmail.com>
2019-12-06 23:38:53 +0900

    Build xml with xmlbuilder

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,17 +7,51 @@ const h = React.createElement
 test('basic structure', () => {
   const element = h('a', {b: 'c'})
   expect(react2xml(element)).toBe(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<a b="c"/>`)
+<a b="c"/>
+`)
 })
 
 test('nest structure', () => {
   const element = h('a', {b: 'c'},
-    h('b'),
-    h('c')
+    h('b', {}, h('d')),
+    h('c', {d: 2}, 'World')
   )
   expect(react2xml(element)).toBe(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <a b="c">
-  <b/>
-  <c/>
-</a>`)
+  <b>
+    <d/>
+  </b>
+  <c d="2">World</c>
+</a>
+`)
+})
+
+test('multi nest structure', () => {
+  const element = h('a', {b: 'c'},
+    h('b', {}, h('d')),
+    h('c', {c: 2}),
+    h('bb', {}, 'Hello'),
+  )
+  expect(react2xml(element)).toBe(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<a b="c">
+  <b>
+    <d/>
+  </b>
+  <c c="2"/>
+  <bb>Hello</bb>
+</a>
+`)
+})
+
+test('multiline text', () => {
+  const element = h('a', {b: 'c'},
+    h('b', {}, `a&
+b`),
+  )
+  expect(react2xml(element)).toBe(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<a b="c">
+  <b>a&amp;
+b</b>
+</a>
+`)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,42 @@
 import testRenderer, { ReactTestRendererJSON } from 'react-test-renderer'
-import xml2js from 'xml2js'
-
-const transformJSON = (element: ReactTestRendererJSON) => {
-  const { props, children } = element
-
-  let childMap = {} as any
-  if (children) {
-    children.forEach(child => {
-      if (typeof child === 'string') {
-        childMap._ = child
-      } else {
-        const current = childMap[child.type]
-        if (current) {
-          childMap[child.type] = [...current, transformJSON(child)]
-        } else {
-          childMap[child.type] = [transformJSON(child)]
-        }
-      }
-    })
-  }
-
-  return {
-    $: props,
-    ...childMap
-  }
-}
+import xmlbuilder, { create } from 'xmlbuilder'
 
 const react2xml = (element: JSX.Element) => {
   const rendererJSON = testRenderer.create(element).toJSON()
   if (!rendererJSON) throw new Error('Element is invalid')
-  const xmlJSON = transformJSON(rendererJSON)
-  const xbuilder = new xml2js.Builder({
-    rootName: rendererJSON!.type
-  });
-  return xbuilder.buildObject(xmlJSON)
+
+  const builder = xmlbuilder.create(rendererJSON.type)
+  buildXML(builder, rendererJSON, true)
+  builder.end({ pretty: true })
+  const result = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' + builder.toString({ pretty: true })
+
+  return result
+}
+
+
+const buildXML = (builder: xmlbuilder.XMLElement, rendererJSON: ReactTestRendererJSON, root: Boolean = false) => {
+  const { type, props, children: _children } = rendererJSON
+  const children = _children || []
+
+  const firstChild = children[0]
+  if (children.length === 1 && typeof firstChild === 'string') {
+    builder.ele(type, props, firstChild)
+    return
+  }
+
+  let xmlElement: xmlbuilder.XMLElement | null = null
+  if (root) {
+    Object.keys(props).map(key => {
+      builder!.att(key, props[key])
+    })
+  } else {
+    xmlElement = builder.ele(type, props)
+  }
+
+  children.forEach((element) => {
+    if (typeof element === 'string') throw new Error('Invaild XML')
+    buildXML(xmlElement || builder, element)
+  })
 }
 
 export default react2xml
